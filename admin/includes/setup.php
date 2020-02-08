f02545c501db7f8fb034e3a6f6be841d8f072f88
Ente <50058585+TheDucky@users.noreply.github.com>
2020-02-08 04:31:44 +0100

    Update v08022020 - v0.1.2.1
    This Update contains several small updates.
    We've added in db.inc.php a confirmation message.

--- admin/includes/setup.php
+++ admin/includes/setup.php
@@ -5,6 +5,16 @@
     $password = $_POST['password'];
     $dname = $_POST['dbase'];
 
-    
- 
+    $conn = mysqli_connect($servername, $username, $password, $dname);
+
+    if (mysqli_connect_error()) {
+        die("Database Connection failed: " . mysqli_connect_error());
+    }
+
+    else {
+
+    echo "Test completed! You can now delete the setup.php and index.php from the admin folder!";
+    fopen("duck.txt", "w");
+    fwrite("Success!");
+};
 ?>
--- includes/db.inc.php
+++ includes/db.inc.php
@@ -6,4 +6,13 @@ $dbPassword = "";  /* And insert here your password, should be clear, huh?*/
 $dbName = ""; /* As well the database*/
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+if (mysqli_connect_error()) {
+    die("Database Connection failed: " . mysqli_connect_error());
+}
+
+else {
+
+echo "Everything Succed!";
+};
 ?>
