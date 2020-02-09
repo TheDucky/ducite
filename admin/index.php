4d77a3c3b329c8ec26f4dac6cd849de10a873dfd
Ente <50058585+TheDucky@users.noreply.github.com>
2020-02-09 15:00:17 +0100

    Update v09022020 - v0.1.3.1
    We've added some small fixes to the .gitattributes file and added the ul.css and index.php file for the admin directory

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,21 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+
+    <html>
+
+    <head>
+            <meta charset="UTF-8">
+            <title>Ducite Admin Panel</title>
+            <link rel="stylesheet" type="text/css" href="css/index.css">
+            <link rel="stylesheet" type="text/css" href="/css/ul.css">
+            <link rel="stylesheet" type="text/css" href="/css/index.css">
+        </head>
+
+        <body>
+            <ul>
+                <li><a href="admin.php">Admin Panel</a></li>
+                <li><a href="creator.php">Create a Webform</a></li>
+                <li><a href="manage.php">Manage your Webforms</a></li>
+                <li><a href="about.php">About Ducite</a></li>
+            </ul>
+        </body>
+    </html>
